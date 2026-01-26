a8eafbefd225531eb44b8d54d2e0f41ec82d0d53
RomaArtur <arturromafurtado@gmail.com>
2026-01-26 17:04:17 -0300

    acesso liberado

--- revisao/exercicios.js
+++ revisao/exercicios.js
@@ -26,5 +26,60 @@ if (salario >= 11000) {
   console.log("9% de bônus");
 }
 
-let atur; 
+let artur; 
+
+// É bissexto ou não? 
+
+const ano = 2026;
+
+if (((ano % 4 === 0) && (ano % 100 !== 0)) || (ano % 400 === 0)) {
+  console.log(`${ano} é bissexto`);
+} else {
+  console.log(`${ano} não é bissexto`);
+};
+
+// Verificar se estudante receberá bônus na nota
+// Estudantes recebem bônus se a nota for 8 ou acima e se tiverem no máximo 2 faltas 
+
+const nome = "Roberta";
+const nota = 8;
+const faltas = 3; 
+
+const recebeBonus = (nota >= 8) && (faltas <= 2) 
+? `${nome} recebe bônus` 
+: `${nome} não recebe bônus`;
+
+console.log(recebeBonus);
+
+// Criar um fluxo que identifica o tipo de usuário e comunica de acordo.
+// ex: usuário free tem acesso limitado ao app
+// usuario premium tem acesso a todas as funções
+// usuário super premium tem acesso total e bônus especiais
+const user = premium;
+
+switch(user) {
+  case 'free': 
+    console.log('acesso limitado');
+    break;
+  case 'premium':
+    console.log('acesso total');
+    break
+  case 'super premium':
+    console.log('acesso total ao app e bônus');
+    break
+  default:
+    console.log('tipo de user desconhecido');
+    break
+};
+
+// controle de acesso para um evento
+// para entrar precisa ter 18 anos ou mais 
+
+const idade = 17;
+
+const acesso = (idade >= 18) 
+? 'Acesso Permitido' 
+: 'Acesso Negado';
+
+
 
